ecddc2cd43906c2665840a2a1f1791a0fcb29af6
David Chau <davidchauprogrammer@gmail.com>
2016-02-18 14:25:04 -0800

    Doing Operating Stuff

--- js/OperatingSystem.js
+++ js/OperatingSystem.js
@@ -1,4 +1,51 @@
 var operatingSystem = function () {
+    "use strict";
     //var qJobQueue = 
     //llIOQueue =
+    var arrDirectory = {};
+
+    //Mode: read or write
+    //Flags
+    //return error code or nothing
+    var open = function (fileName, mode) {
+        if (fileName in arrDirectory) {
+            return 1;
+        } else {
+            return -1;
+        }
+    };
+    //Flags, returns an error code
+    var close = function (fileName) {
+        
+    };
+    var create = function (fileName, contents) {
+        arrDirectory[fileName] = contents.match(/.{1,100}/g);
+    };
+    //returns an error code
+    var delet = function (fileName) {
+        if (fileName in arrDirectory) {
+            delete arrDirectory[fileName];
+            return 1;
+        } else {
+            return -1;
+        }
+    };
+    //return num of Bytes / length of string
+    var read = function (fileName, position) {
+        return arrDirectory[fileName][position].length();
+    };
+    //return num of bytes / length of string
+    var write = function (filePointer, string) {
+        
+    };
+    var length = function () {
+        
+    };
+    var seek = function (position) {
+        
+    };
+    var position = function () {
+        
+    };
+    
 };
--- js/TestIO.js
+++ js/TestIO.js
@@ -1,24 +1,11 @@
 /*global findContact*/
-var aryContactCsv = ["David Chau", ",", "George Jone", ",", "Phuc Chau", ",", "Sa Chau", ",",
-                  "Hung Do", ",", "Darin Vergara", ",", "Sam Gluss", ",", "Em Santos", ",",
-                  "Nhan Nguyen", ",", "Thomas Nguyen", ",", "Tony Tran", ",", "Paul Klein", ",",
-                  "Brook Thomas", ",", "Gilbert Bui", ",", "Hari Mannivan", ",", "Eric Chen", ",",
-                  "Allen Space", ",", "Ivan Gonzalez", ",", "Kenny Luo", ",", "Nicu Listana", ",",
-                  "Khanh Le", ",", "Kenny Chao", ",", "Dan Roberts", ",", "Anna Sever", ",",
-                  "Tricia Nemiroff", ",", "Chris Lang", ",", "Thomas Tse", ",", "Benson Zhang", ",",
-                  "Sang Saephan", ",", "Chitose Kagami", ",", "Derek Bantug", ",", "Linden Chiu", ",",
-                  "Roger Schrag", ",", "Jay Chan", ",", "Tai-An Cha", ",", "Tom Rike", ",",
-                  "Kevin Wong", ",", "Jimmy Tran", ",", "Josh Johansen", ",", "Bryan Chen", ",",
-                  "Randy Phan", ",", "Joyce Yee", ",", "Abigail Jem", ",", "Tiffany Ku", ",",
-                  "Alvin Lu", ",", "Matt Wischoff", ",", "Camille Davis", ",", "Herp Derp", ",",
-                  "Frank Koutoulas", ",", "Anthony Souza"];
-
 var contactManager = null;
 
 function testingInputOutput() {
     "use strict";
 //    var contactManager = new ContactManager();
 //    var test = contactManager.findContact(aryContactCsv);
-    var test = findContact(aryContactCsv);
+    //var test = findContact(aryContactCsv);
+    var derp = new File();
     return test;
 }
